c459486137af2fa09b7e756e36fb7a9011a38cdb
0xTab <tab.liu@sosovalue.com>
2025-08-24 23:22:59 +0800

    feat: builder.io builder content5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Content1 from "@/components/Content1";
 import Content2 from "@/components/Content2";
 import Content3 from "@/components/Content3";
 import Content4 from "@/components/Content4";
+import Content5 from "@/components/Content5";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <Content2 />
         <Content3 />
         <Content4 />
+        <Content5 />
         {/* Additional content sections will be added here */}
       </main>
     </div>
