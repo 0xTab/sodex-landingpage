daec8d0ffaa4092caa2d6e88e7f769f7e328b191
0xTab <tab.liu@sosovalue.com>
2025-08-25 18:05:14 +0800

    fix: updat mobile content2

--- src/components/Content2.tsx
+++ src/components/Content2.tsx
@@ -74,11 +74,11 @@ export default function Content2() {
   return (
     <section className="w-full mobile:h-[940px]  pc:h-[866px] pc:px-[6.5%] pc:py-[6.5%] overflow-hidden bg-gradient-to-b from-primary via-primary/95 to-[#212121">
       {/* Content */}
-      <div className="relative z-10 flex h-full items-start justify-center mobile:px-10 mobile:pt-20">
+      <div className="relative z-10 flex h-full items-start justify-center mobile:px-10 mobile:pt-[30px]">
         <div className="flex w-full mobile:max-w-[313px] pc:max-w-[1200px] flex-col items-center mobile:gap-4 pc:gap-[52px]">
 
           {/* Title */}
-          <div className="text-center mobile:pb-8 pc:pb-0">
+          <div className="text-center pc:pb-0">
             <h2 className="text-white mobile:text-[28px] pc:text-4xl font-latoBold leading-normal mobile:max-w-[313px] pc:max-w-4xl mx-auto">
               SoDEX — The next‑gen onchain exchange
             </h2>
@@ -88,7 +88,7 @@ export default function Content2() {
           <div className="mobile:block pc:hidden w-full max-w-[243px] relative">
             <div className="relative rounded-[4.93px] overflow-hidden">
               <Image
-                src="https://api.builder.io/api/v1/image/assets/TEMP/e7c52c0ebd105717400190b2981a05cddea2edba?width=442"
+                src="/img/home/mobile-content2-inner.webp"
                 alt="SoDEX Trading Interface"
                 width={221}
                 height={480}
@@ -101,14 +101,14 @@ export default function Content2() {
           </div>
 
           {/* Features Section */}
-          <div className="mobile:block pc:hidden w-full">
-            <div className="mobile:px-6 mobile:py-6 pc:px-0 pc:py-0 border-t border-border-primary-100700 bg-gradient-to-b from-primary to-[#212121]">
-              <div className="flex flex-col mobile:gap-6 pc:gap-8">
+          <div className="mobile:block pc:hidden w-full absolute bottom-0">
+            <div className="mobile:px-6 mobile:py-6 pc:px-0 pc:py-0 border-t-[1px] border-[#262626]  bg-gradient-to-b from-primary to-[#212121]">
+              <div className="flex flex-col mobile:gap-6 pc:gap-8  ">
                 {features.map((feature, index) => (
                   <div key={index}>
                     <FeatureCard feature={feature} />
                     {index < features.length - 1 && (
-                      <div className="h-px bg-border-primary-100700 mobile:mt-6 pc:mt-8"></div>
+                      <div className="h-[1px] bg-[#262626] mobile:mt-[4px] pc:mt-8"></div>
                     )}
                   </div>
                 ))}
